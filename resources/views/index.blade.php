799ba55720514ee529b584a6d86b81781cd8ee9f
gabriel <gabrieldj1997@gmail.com>
2021-09-21 13:56:49 -0300

    repositorio laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,393 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Eccentric portfolio - personal website template | Home : W3layouts</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/style-starter.css">
+
+</head>
+
+<body>
+<div class="w3l-header" id="home">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark pl-0 pr-0">
+            <a class="navbar-brand m-0 text-primary" href="/"><span class="fa fa-gamepad"></span> Eccentric </a> 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active mr-lg-4">
+                        <a class="nav-link pl-0 pr-0 font-weight-bold" href="/">Home <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item mr-lg-4">
+                        <a class="nav-link pl-0 pr-0 font-weight-bold" href="/about">About</a>
+                    </li>
+                    <li class="nav-item mr-lg-4">
+                        <a class="nav-link pl-0 pr-0 font-weight-bold" href="/services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pl-0 pr-0 font-weight-bold" href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+
+<div class="w3l-banner-slider">
+    <div class="wrapper-container">
+ 
+        <main class="sliders-container">
+
+            <ul class="pagination">
+                <li class="pagination__item"><a class="pagination__button"></a></li>
+                <li class="pagination__item"><a class="pagination__button"></a></li>
+                <li class="pagination__item"><a class="pagination__button"></a></li>
+                <li class="pagination__item"><a class="pagination__button"></a></li>
+            </ul>
+            <div class="social">
+                <a class="" href="#facebook"><span class="fa fa-facebook"></span></a>
+                <a class="" href="#twitter"><span class="fa fa-twitter"></span></a>
+                <a class="" href="#instagram"><span class="fa fa-instagram"></span></a>
+            </div>
+        </main>
+    </div>
+</div>
+
+<div class="w3l-content-photo-5 py-5">
+	<div class="content-main py-lg-5 py-md-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 content-left">
+					<h3>I Specialize in Branding and Strategy, and am passionate
+						about creating Awesome Portfolio work.</h3>
+					<p class="mb-0">Donec luctus rhoncus dignissim. Integer blandit mattis arcu, id viverra orci condimentum a.
+						Quisque nibh tortor, mollis placerat semper ac, auctor quis mauris. Praesent dignissim sed
+						magna eu urna consectetur, at pretium nisi aliquet. Sed vestibulum malesuada semper. Init
+						Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec malesuada nibh sed dolorinet
+						libero posuere, sed cursus elit congue. Sed vestibulum malesuada semper. Init
+						Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec malesuada nibh sed dolorinet
+						libero posuere, sed cursus elit congue donec malesuada nibhe</p>
+					<a href="#portfolio" class="primary-btn-style btn-primary btn mt-lg-5 mt-4">My live work</a>
+				</div>
+				<div class="col-lg-4 content-photo mt-lg-0 mt-sm-5 mb-md-0 mb-4">
+					<a href="#img"><img src="assets/images/myimage.jpg" class="img-fluid" alt="content-photo"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="w3l-gallery-7 bg-light py-5">
+	<div class="gallery-7_sur py-lg-5">
+		<div class="container">
+			<div id="container">
+				<div class="galleryContainer">
+					<div class="gallery">
+						<input type="radio" name="controls" id="c1" checked><img class="galleryImage img-fluid" id="i1"
+							src="assets/images/g1.jpg" class="img" alt="">
+						<input type="radio" name="controls" id="c2"><img class="galleryImage img-fluid" id="i2"
+							src="assets/images/g2.jpg" class="img" alt="">
+						<input type="radio" name="controls" id="c3"><img class="galleryImage img-fluid" id="i3"
+							src="assets/images/g3.jpg" class="img" alt="">
+						<input type="radio" name="controls" id="c4"><img class="galleryImage img-fluid" id="i4"
+							src="assets/images/g4.jpg" class="img" alt="">
+						<input type="radio" name="controls" id="c5"><img class="galleryImage img-fluid" id="i5"
+							src="assets/images/g5.jpg" class="img" alt="">
+						<input type="radio" name="controls" id="c6"><img class="galleryImage img-fluid" id="i6"
+							src="assets/images/g6.jpg" class="img" alt="">
+					</div>
+					<div class="thumbnails">
+						<label class="thumbnailImage" for="c1"><img src="assets/images/g1.jpg" class="img img-fluid"
+								alt=""></label>
+						<label class="thumbnailImage" for="c2"><img src="assets/images/g2.jpg" class="img img-fluid"
+								alt=""></label>
+						<label class="thumbnailImage" for="c3"><img src="assets/images/g3.jpg" class="img img-fluid"
+								alt=""></label>
+						<label class="thumbnailImage" for="c4"><img src="assets/images/g4.jpg" class="img img-fluid"
+								alt=""></label>
+						<label class="thumbnailImage" for="c5"><img src="assets/images/g5.jpg" class="img img-fluid"
+								alt=""></label>
+						<label class="thumbnailImage" for="c6"><img src="assets/images/g6.jpg" class="img img-fluid"
+								alt=""></label>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="w3l-blog py-5">
+    <div class="container py-lg-5">
+        <div class="row">
+            <div class="col-lg-3 col-6 pr-md-3 pr-2">
+                <div class="img-block">
+                    <a href="#img">
+                        <img src="assets/images/iphone1.png" class="img-fluid" alt="image" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6 pl-md-3 pl-2">
+                <div class="img-block">
+                    <a href="#img">
+                        <img src="assets/images/iphone2.png" class="img-fluid" alt="image" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 mt-lg-0 mt-md-5 mt-4">
+                <h3>Sed volutpat eget dui ut tempus init bibendum nunc.</h3>
+                <h5 class="mt-3">Fusce fringilla tincidunt laoreet volutpat cras varius sit suscipit.</h5>
+                <p class="mt-4 mb-0"> Sed in metus libero. Sed volutpat eget dui ut tempus. Fusce fringilla tincidunt laoreet
+                    Morbi ac metus vitae diam scelerisque malesuada eget eu mauris. Cras varius lorem ac velit pharetra,
+                    non scelerisque mi vulputate. Phasellus bibendum suscipit nunc, non semper erat iaculis in. Nulla
+                    volutpat porttitor magna vel euismod. Aenean sit amet diam nec sem
+                    amet metus.</p>
+                <a href="#blog.html" class="primary-btn-style btn-primary btn mt-lg-5 mt-4">View my project</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="w3l-quote-main">
+    <div class="quote py-5">
+        <div class="container py-lg-5">
+            <div class="row">
+                <div class="col-lg-9">
+                    <h4>" I've worked with some X company for several years and that is the best company in the Market.
+                        Mauris elementum tortor a nisl
+                        aliquet fringilla. Donec et risus augue. Proin sit amet maximus dui Vivamus lacinia nisi tempor
+                        ligula sagittis vehicula."</h4>
+                </div>
+                <div class="col-lg-3 mt-lg-0 mt-3 text-lg-center tablet-grid">
+                    <img src="assets/images/client2.jpg" alt="" class="img-fluid" />
+                    <div>
+                        <h6 class="mb-0 mt-lg-3">Johnson smith</h6>
+                        <h5 class="mt-1">- UI/UX Designer </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="w3l-blog py-5 bg-light">
+    <div class="container py-lg-5 py-md-3">
+        <div class="row">
+            <div class="col-lg-5">
+                <h3>Sed volutpat eget dui ut tempus init.</h3>
+                <h5 class="mt-3">Fusce fringilla tincidunt laoreet volutpat cras varius sit </h5>
+                <p class="mt-4"> Sed in metus libero. Sed volutpat eget dui ut tempus. Fusce fringilla tincidunt laoreet
+                    Morbi ac metus vitae diam scelerisque malesuada eget eu mauris. Cras varius lorem ac velit pharetra,
+                    non scelerisque mi vulputate. Phasellus bibendum suscipit nunc, non semper erat iaculis in. Nulla
+                    volutpat porttitor magna vel euismod. Aenean sit amet diam nec sem
+                    amet metus.</p>
+            </div>
+            <div class="col-lg-7 mt-lg-0 mt-4">
+                <div class="img-block">
+                    <a href="#single">
+                        <img src="assets/images/g1.jpg" class="img-fluid" alt="image" />
+                        <span>Modern Ecommerce Website Design</span>
+                    </a>
+                </div>
+                <div class="img-block mt-3">
+                    <a href="#single"> <img src="assets/images/g2.jpg" class="img-fluid" alt="image" />
+                        <span>Personalized Portfolio work</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="w3l-grid-quote py-5">
+    <div class="container py-lg-3">
+        <h6>I'am available for freelance projects.</h6>
+        <h3>Let's work together indeed!</h3>
+        <a href="/contact" class="secondary-btn-style btn-secondary btn">Get quotes</a>
+        <a href="/contact" class="btn btn-style text-primary ml-2">Hire me</a>
+    </div>
+</section>
+
+<section class="w3l-footers-1">
+	<div class="footer bg-secondary">
+		<div class="container">
+			<div class="footer-content">
+				<div class="row">
+					<div class="col-lg-8 footer-left">
+						<p class="m-0">© Copyright 2020 Eccentric Portfolio. Design by <a
+								href="https://w3layouts.com">W3layouts</a></p>
+					</div>
+					<div class="col-lg-4 footer-right text-lg-right text-center mt-lg-0 mt-3">
+						<ul class="social m-0 p-0">
+							<li><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+							<li><a href="#linkedin"><span class="fa fa-linkedin"></span></a></li>
+							<li><a href="#instagram"><span class="fa fa-instagram"></span></a></li>
+							<li><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<button onclick="topFunction()" id="movetop" class="bg-primary" title="Go to top">
+	<span class="fa fa-angle-up"></span>
+</button>
+<script>
+
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("movetop").style.display = "block";
+		} else {
+			document.getElementById("movetop").style.display = "none";
+		}
+	}
+
+
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+</script>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+
+<script src="assets/js/momentum-slider.min.js"></script>
+<script>
+  (function () {
+
+    var slidersContainer = document.querySelector('.sliders-container');
+
+
+    var msNumbers = new MomentumSlider({
+      el: slidersContainer,
+      cssClass: 'ms--numbers',
+      range: [1, 4],
+      rangeContent: function (i) {
+        return '0' + i;
+      },
+      style: {
+        transform: [{
+          scale: [0.4, 1]
+        }],
+        opacity: [0, 1]
+      },
+
+      interactive: false
+    });
+
+
+
+    var titles = [
+      'Branding ',
+      'Awesome',
+      'Creative',
+      'Portfolio'
+    ];
+
+    var msTitles = new MomentumSlider({
+      el: slidersContainer,
+      cssClass: 'ms--titles',
+      range: [0, 3],
+      rangeContent: function (i) {
+        return '<h3>' + titles[i] + '</h3>';
+      },
+      vertical: true,
+      reverse: true,
+      style: {
+        opacity: [0, 1]
+      },
+
+      interactive: false
+    });
+
+
+
+    var msLinks = new MomentumSlider({
+      el: slidersContainer,
+      cssClass: 'ms--links',
+      range: [0, 3],
+      rangeContent: function () {
+        return '<a href="/about" class="ms-slide__link btn">View More</a>';
+      },
+      vertical: true,
+      interactive: false
+    });
+
+
+
+    var pagination = document.querySelector('.pagination');
+    var paginationItems = [].slice.call(pagination.children);
+
+
+    var msImages = new MomentumSlider({
+
+      el: slidersContainer,
+
+      cssClass: 'ms--images',
+
+      range: [0, 3],
+      rangeContent: function () {
+        return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+      },
+
+      sync: [msNumbers, msTitles, msLinks],
+
+      style: {
+        '.ms-slide__image': {
+          transform: [{
+            scale: [1.5, 1]
+          }]
+        }
+      },
+
+
+
+      change: function (newIndex, oldIndex) {
+        if (typeof oldIndex !== 'undefined') {
+          paginationItems[oldIndex].classList.remove('pagination__item--active');
+        }
+        paginationItems[newIndex].classList.add('pagination__item--active');
+      }
+    });
+
+
+
+    pagination.addEventListener('click', function (e) {
+      if (e.target.matches('.pagination__button')) {
+        var index = paginationItems.indexOf(e.target.parentNode);
+        msImages.select(index);
+      }
+    });
+
+  })();
+</script>
+
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script>
+  $(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| Contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/services', function(){
+    return view('services');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
